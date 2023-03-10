12a14f1758a1f0f0f5339dc6a8212653631bf796
NRF <nrf@asdf.systems>
2023-03-10 14:45:00 +1300

    Remove NAT gateway

--- main.tf
+++ main.tf
@@ -3,24 +3,20 @@ provider "aws" {
   region = "us-east-2"
 }
 
-
 # Create a VPC with 3 public and 3 private subnets
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "neil-devops-vpc"
-  cidr = "10.0.0.0/16"
-
+  name            = "neil-devops-vpc"
+  cidr            = "10.0.0.0/16"
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-
-  # Allow auto-assign public IP on launce
+  # Allow auto-assign public IP on launch
   map_public_ip_on_launch = true
-  # Create an Internet Gateway for this VPC
-  enable_nat_gateway = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
 
 
   tags = {
@@ -30,7 +26,6 @@ module "vpc" {
 }
 
 # Create a security group to allow SSH access to the EC2 instance
-
 resource "aws_security_group" "ssh_access" {
   name_prefix = "ssh-access"
   vpc_id      = module.vpc.vpc_id
@@ -55,7 +50,7 @@ resource "aws_security_group" "ssh_access" {
   }
 }
 
-# Create a security group to allow internet egress the EC2 instance
+# Create a security group to allow internet egress from the EC2 instance
 resource "aws_security_group_rule" "egress_internet" {
   security_group_id = aws_security_group.ssh_access.id
 
@@ -67,14 +62,12 @@ resource "aws_security_group_rule" "egress_internet" {
 }
 
 # Add a key pair to the EC2 instance
-
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "nrf@grunt"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
 # Create an EC2 instance
-
 resource "aws_instance" "devops-instance" {
   ami           = "ami-00eeedc4036573771"
   instance_type = "t2.micro"
@@ -82,17 +75,11 @@ resource "aws_instance" "devops-instance" {
   security_groups = [aws_security_group.ssh_access.id]
   key_name      = aws_key_pair.my_key_pair.key_name
 
-
-  ebs_block_device {
-    device_name = "/dev/xvda"
-    volume_size = 8
-  }
-
+  # Assign a public IP address directly to the instance
+  associate_public_ip_address = true
 
   tags = {
     Name        = "DevOps-Instance"
     Environment = "dev"
   }
 }
-
-
