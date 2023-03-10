8ebe851c9e373fd078f5cb2265dda153dcfc599f
NRF <nrf@asdf.systems>
2023-03-10 21:23:33 +1300

    Pervasive Instance

--- main.tf
+++ main.tf
@@ -13,15 +13,14 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   # Allow auto-assign public IP on launch
-  map_public_ip_on_launch = true
-  enable_nat_gateway = false
-  single_nat_gateway = false
-
-
+  map_public_ip_on_launch          = true
+  enable_nat_gateway               = false
+  single_nat_gateway               = false
+  default_vpc_enable_dns_hostnames = true
 
   tags = {
-    Terraform    = "true"
-    Environment  = "dev"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
 
@@ -64,16 +63,16 @@ resource "aws_security_group_rule" "egress_internet" {
 # Add a key pair to the EC2 instance
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "nrf@grunt"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 # Create an EC2 instance
 resource "aws_instance" "devops-instance" {
-  ami           = "ami-00eeedc4036573771"
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc.public_subnets[0]
-  security_groups = [aws_security_group.ssh_access.id]
-  key_name      = aws_key_pair.my_key_pair.key_name
+  ami                         = "ami-00eeedc4036573771"
+  instance_type               = "t3a.micro"
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.ssh_access.id]
+  key_name                    = aws_key_pair.my_key_pair.key_name
   associate_public_ip_address = true
 
   tags = {
@@ -84,4 +83,50 @@ resource "aws_instance" "devops-instance" {
   provisioner "local-exec" {
     command = "ansible-playbook -i '${aws_instance.devops-instance.public_ip},' playbook.yml --private-key ~/.ssh/id_rsa --user ubuntu"
   }
+}
+
+resource "aws_instance" "pervasive-instance" {
+  ami                         = "ami-02238ac43d6385ab3"
+  instance_type               = "t3a.micro"
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.ssh_access.id]
+  key_name                    = aws_key_pair.my_key_pair.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name        = "Pervasive-Instance"
+    Environment = "dev"
+  }
+
+  #provisioner "file" {
+  #  source = "install/PSQL-13.31-026.000.x86_64.rpm"
+  #  destination = "/tmp/PSQL-13.31-026.000.x86_64.rpm"
+
+  #  connection {
+  #    type     = "ssh"
+  #    user     = "ubuntu"
+  #    private_key = file("~/.ssh/id_rsa")
+  #    host     = self.public_ip
+  #  }
+  #}
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${aws_instance.pervasive-instance.public_ip},' playbook-pervasive.yml --private-key ~/.ssh/id_rsa --user ec2-user"
+  }
+}
+
+resource "aws_route53_record" "a_nrf_asdf_co_nz" {
+  zone_id = "Z02645693VHQRFVDHHYY4"
+  name    = "nrf.asdf.co.nz"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.devops-instance.public_ip]
+}
+
+resource "aws_route53_record" "a_psql_asdf_co_nz" {
+  zone_id = "Z02645693VHQRFVDHHYY4"
+  name    = "psql.asdf.co.nz"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.pervasive-instance.public_ip]
 }
